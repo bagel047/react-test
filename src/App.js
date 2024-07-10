295c38e606992beeb06776fca1068883babaec61
bagel047e <bage.josifoska@gmail.com>
2024-07-10 10:49:10 +0200

    add tailwind

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import './App.css';
+import './index.css';
 import Employee from './components/Employee';
+import { useState } from 'react';
 
 function App() {
+  const [role, setRole] = useState('dev');
   const showEmployees = true;
   return (
     <div className="App">
 
       {showEmployees ?
         (<>
+          <label className="bg-blue-200">Role: </label>
+          <input className="bg-blue-200" type='text' onChange={(e) => {
+            console.log(e.target.value);
+            setRole(e.target.value);
+          }}></input>
           <Employee name="John" role="Intern" />
-          <Employee name="Abby" />
+          <Employee name="Abby" role={role} />
           <Employee name="Bot" />
         </>) : (<p>Show employees is off</p>)}
     </div>
