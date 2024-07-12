33e3789ac0b42115cd77bc1074ad9c6e38f5053d
bagel047e <bage.josifoska@gmail.com>
2024-07-12 10:53:11 +0200

    edit employee

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [role, setRole] = useState('dev');
-  const showEmployees = true;
+  const [employees, setEmployees] = useState(
+    [
+      { id: 1, name: "John", role: "Intern", img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg" },
+      { id: 2, name: "Abby", role: "Dev", img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg" },
+      { id: 3, name: "David", role: "Manager", img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg" },
+      { id: 4, name: "Sarah", role: "HR", img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg" },
+      { id: 5, name: "James", role: "Intern", img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg" },
+      { id: 6, name: "Travis", role: "Dev", img: "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg" }
+    ]
+  );
+
+  function updateEmployee(id, newName, newRole) {
+
+    const updatedEmployees = employees.map((employee) => {
+      if (id == employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
   return (
     <div className="App">
+      <>
+        <div className="flex flex-wrap container mx-auto bg-slate-400 justify-center mt-12 p-6 rounded">
+          {employees.map(employee => {
+            return <Employee key={employee.id} id={employee.id} name={employee.name} role={employee.role} img={employee.img} updateEmployee={updateEmployee} />
+          })}
+        </div>
+      </>
 
-      {showEmployees ?
-        (<>
-          <label className="bg-blue-200">Role: </label>
-          <input className="bg-blue-200" type='text' onChange={(e) => {
-            console.log(e.target.value);
-            setRole(e.target.value);
-          }}></input>
-          <Employee name="John" role="Intern" />
-          <Employee name="Abby" role={role} />
-          <Employee name="Bot" />
-        </>) : (<p>Show employees is off</p>)}
     </div>
   );
 }
--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,18 +1,21 @@
+import EditEmployee from './EditEmployee'
+
 function Employee(props) {
     return (
         <>
-            <div className="my-8 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-                <img className="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="/img/erin-lindford.jpg" alt="Woman's Face" />
+            <div className="min-w-[350px] max-w-[350px] mx-4 my-4 py-8 px-8 max-w-sm w-3/12 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+                <img className="rounded-full object-cover h-[100px] w-[100px] block mx-auto h-24 sm:mx-0 sm:shrink-0" src={props.img} />
                 <div className="text-center space-y-2 sm:text-left">
                     <div className="space-y-0.5">
                         <p className="text-lg text-black font-semibold">
-                            Erin Lindford
+                            {props.name}
                         </p>
                         <p className="text-slate-500 font-medium">
-                            Product Engineer
+                            {props.role ? props.role : "No role"}
                         </p>
                     </div>
-                    <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+
+                    <EditEmployee id={props.id} name={props.name} role={props.role} updateEmployee={props.updateEmployee} />
                 </div>
             </div>
         </>
